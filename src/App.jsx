11516fbcc7799af76032e7a85f8e41023a81499e
hadriansilva-cps <hadrians746@gmail.com>
2025-05-10 10:32:26 -0300

    avaliação

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,32 @@ import Footer from './components/Footer'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import { useAuthValue } from './context/AuthContext';
+import Dashboard from './pages/Dashboard/Dashboard';
+import About  from './pages/About/About';
+import CreatePost from './pages/CreatePost/CreatePost';
 import './App.css'
+  
 
 function App() {
-
+  const { user } = useAuthValue();
+ 
   return (
     <>
+       
       <div>
+        
         <BrowserRouter>
+        
         <Navbar />
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/"/>} />
+              <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/Create-post" element={<CreatePost />} />
             </Routes>
           </div>
           <Footer />
@@ -28,3 +40,6 @@ function App() {
 }
 
 export default App
+
+
+
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/config";
+import PropTypes from 'prop-types'
+
+
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false)
+        })
+
+        return () => unsubscribe();
+
+        
+    }, []);
+
+    return(
+        <AuthContext.Provider value={{ user, loading }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export function useAuthValue(){
+    return useContext(AuthContext);
+}
